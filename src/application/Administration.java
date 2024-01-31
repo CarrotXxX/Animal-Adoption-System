1abaed80fc533fb8fabe4a10b1d36f78b900ecd9
nancyluo <luonancy74@gmail.com>
2024-01-31 15:21:02 +1300

    final version

--- src/application/Administration.java
+++ src/application/Administration.java
@@ -1,12 +1,10 @@
 package application;
 
 public class Administration {
-	
 	private int ID;
 	private String loginUsername;
 	private String loginEmail;
 	private String loginPassword;
-	
 	
 	public void Administration() {}
 
